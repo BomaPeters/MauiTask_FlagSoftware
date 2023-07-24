d41dfe79eaa47aa0ba858b75c3e25c9403378327
BomaPeters <bomapeters@yahoo.co.uk>
2023-07-24 14:58:43 +0100

    The MainPage can  now load a rest api

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,39 @@
-﻿namespace MauiTask_FlagSoftware;
+﻿using MauiTask_FlagSoftware.Models;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+namespace MauiTask_FlagSoftware;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private const string apiUrl = "https://dummyjson.com/products?Limit=10";
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
+		LoadData();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	List<Product> productsList = new List<Product>();
+
+	private  async void LoadData()
 	{
-		count++;
+	HttpClient	client =new HttpClient();//Create Httpclient
+		HttpResponseMessage response= await client.GetAsync(apiUrl);//retrieves response with GET VERB
+	
+		if (response.IsSuccessStatusCode)
+		{
+			string JsonString= await response.Content.ReadAsStringAsync();
+			ProductVM productVM= JsonConvert.DeserializeObject<ProductVM>(JsonString);//Deserialize the jsonstring as an object
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+			foreach(Product product in productVM.products)
+			{
+				productsList.Add(new Product() { Id = product.Id, title = product.title, description = product.description, price = product.price, rating = product.rating, stock = product.stock, discountPercentage = product.discountPercentage, brand = product.brand, category = product.category, thumbnail = product.thumbnail, images = product.images });
+			}
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+			productListView.ItemsSource= productsList;
+		}
+	
 	}
+
 }
 
---
+++ Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTask_FlagSoftware.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+        public decimal discountPercentage { get; set; }
+        public decimal rating { get; set; }
+        public int stock { get; set; }
+        public string brand { get; set; }
+        public string category { get; set; }
+        public string thumbnail { get; set; }
+        public List<string> images { get; set; }
+    }
+    public class ProductVM
+    {
+        public List<Product> products { get; set; }
+        public int total { get; set; }
+        public int skip { get; set; }
+        public int limit { get; set; }
+    }
+}
